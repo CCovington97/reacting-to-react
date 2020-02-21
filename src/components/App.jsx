ffad2bd54f9b07f5cc61e2c8da1a332683e8a315
Connor <ccovington439@gmail.com>
2020-02-21 16:16:08 -0600

    steps 4-6 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,16 @@ class App extends Component {
         super(props);
 
         this.state = {
-
+            text: "some string as the value"
         }
     }
 
     render() {
         return (
-        <h1>{this.props.name}</h1>
+            <React.Fragment>
+                <h1>{this.props.name}</h1>
+                <h2>{this.state.text}</h2>
+            </React.Fragment>
         )
     }
 }
